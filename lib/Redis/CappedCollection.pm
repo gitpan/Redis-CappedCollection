4e401c58a04e01f97558f438eef7e5cf6a8330ba
Sergey Gladkov <sgladkov@cpan.org>
2014-05-27 13:15:29 +0000

    Import of SGLADKOV/Redis-CappedCollection-0.16 from CPAN.
    gitpan-cpan-distribution: Redis-CappedCollection
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         SGLADKOV/Redis-CappedCollection-0.16.tar.gz
    gitpan-cpan-author:       SGLADKOV
    gitpan-cpan-maturity:     released

--- lib/Redis/CappedCollection.pm
+++ lib/Redis/CappedCollection.pm
@@ -7,7 +7,7 @@ a auto-FIFO age-out feature.
 
 =head1 VERSION
 
-This documentation refers to C<Redis::CappedCollection> version 0.15_1
+This documentation refers to C<Redis::CappedCollection> version 0.16
 
 =cut
 
@@ -20,7 +20,7 @@ use bytes;
 
 # ENVIRONMENT ------------------------------------------------------------------
 
-our $VERSION = '0.15_1';
+our $VERSION = '0.16';
 
 use Exporter qw(
     import
